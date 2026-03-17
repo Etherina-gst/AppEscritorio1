b860c9e76ebc478f58f5b794c03c9bc3c700756f
Etherina <senageremias20@gmail.com>
2026-03-17 13:10:38 -0300

    First Commit

---
+++ AppEscritorio1/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppEscritorio1
+{
+    public partial class AppEscritorio1 : Form
+    {
+        public AppEscritorio1()
+        {
+            InitializeComponent();
+        }
+    }
+}
