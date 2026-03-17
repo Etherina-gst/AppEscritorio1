c1a04bf2495a03339b40acdfe5c203dac487bc40
Etherina <senageremias20@gmail.com>
2026-03-17 16:26:25 -0300

    Added .gitignore

--- AppEscritorio1/Form1.Designer.cs
+++ AppEscritorio1/Form1.Designer.cs
@@ -28,20 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.LabelNombre = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // LabelNombre
+            // 
+            this.LabelNombre.AutoSize = true;
+            this.LabelNombre.ForeColor = System.Drawing.Color.Crimson;
+            this.LabelNombre.Location = new System.Drawing.Point(52, 57);
+            this.LabelNombre.Name = "LabelNombre";
+            this.LabelNombre.Size = new System.Drawing.Size(130, 16);
+            this.LabelNombre.TabIndex = 0;
+            this.LabelNombre.Text = "Geremias Was Here";
+            this.LabelNombre.Click += new System.EventHandler(this.label1_Click);
             // 
             // AppEscritorio1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LabelNombre);
             this.Name = "AppEscritorio1";
             this.Text = "AppEscritorio1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LabelNombre;
     }
 }
 
